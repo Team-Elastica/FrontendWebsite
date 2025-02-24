242d1bb54557e1038b8f014b54e810b25bfc87a6
Beal Norazilan <norazilan.1@buckeyemail.osu.edu>
2025-02-24 14:51:01 -0500

    Result section first try

--- fmdb-app/src/App.jsx
+++ fmdb-app/src/App.jsx
@@ -1,12 +1,20 @@
 import { useState } from 'react'
 import './App.css'
 import Header from './Header';
+import Result from './Result';
+// import Card from './assets/components/MediaCard';
 
 function App() {
   return (
     <div>
-      <Header />
+      <section>
+        <Header />
+      </section>
+      <section>
+        <Result />
+      </section>
     </div>
+    
   );
 }
 
